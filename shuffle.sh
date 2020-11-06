ca9776d089d89c9b3ec3c33eb975caffcd7d9b23
Dael Maselli <dmaselli@infn.it>
2020-11-06 14:32:22 +0100

    added

---
+++ shuffle.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export SPOTIPY_CLIENT_ID='5fa95b0eede04f39b6fd485665ae2dd5'
+export SPOTIPY_CLIENT_SECRET='c3012fcde21f4d53829392d382fa12e9'
+export SPOTIPY_REDIRECT_URI='https://www.daelmaselli.com/'
+
+python3 shuffle.py 'dael_pongo' 'spotify:playlist:37i9dQZF1DWTpgpHHF8zH5' 'spotify:playlist:44gcAkitQpSNOdVGcsPWda'
+
