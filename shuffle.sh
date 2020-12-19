2a979591333def1d205fa51f25b5cb9df2ebe79f
Dael Maselli <dmaselli@mb15dael.dsd>
2020-12-19 12:07:08 +0100

    cd dir

--- shuffle.sh
+++ shuffle.sh
@@ -3,7 +3,7 @@ export SPOTIPY_CLIENT_ID='5fa95b0eede04f39b6fd485665ae2dd5'
 export SPOTIPY_CLIENT_SECRET='c3012fcde21f4d53829392d382fa12e9'
 export SPOTIPY_REDIRECT_URI='https://www.daelmaselli.com/'
 
-# la vita é bella - spotify:playlist:37i9dQZF1DX9qUVnHy7VXV
+cd `dirname $0`
 
 python3 shuffle.py 'dael_pongo' 'spotify:playlist:37i9dQZF1DWTpgpHHF8zH5' 'spotify:playlist:37i9dQZF1DX9qUVnHy7VXV' 'spotify:playlist:44gcAkitQpSNOdVGcsPWda'
 
